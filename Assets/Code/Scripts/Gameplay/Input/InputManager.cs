a07d3217ea1ddffe1e953da5d820ed808b4e88d3
FlavioNovati <f.novati10@gmail.com>
2024-02-28 12:59:23 +0100

    Initial Input Settings

---
+++ Assets/Code/Scripts/Gameplay/Input/InputManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManagr : MonoBehaviour
+{
+    [SerializeField] private InputMap InputMapping;
+
+}
