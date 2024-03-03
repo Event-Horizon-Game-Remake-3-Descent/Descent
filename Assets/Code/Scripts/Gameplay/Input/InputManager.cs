64d6500753d5981166af78477c053cd54932776b
DrNox08 <marcopergamena.01@gmail.com>
2024-03-03 19:32:25 +0100

    Added: Mouse Input + Smoothing, Back and Forw input

--- Assets/Code/Scripts/Gameplay/Input/InputManager.cs
+++ Assets/Code/Scripts/Gameplay/Input/InputManager.cs
@@ -2,8 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManagr : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
-    [SerializeField] private InputMap InputMapping;
+    public static InputMap InputMap;
+    public InputMap prova;
+
+    public static InputManager Instance;
+
+    public static Vector2 MovementInput => InputMap.Overworld.Movement.ReadValue<Vector2>();
+
+    private void Awake()
+    {
+        InputMap = new InputMap();
+
+    }
+    private void OnEnable()
+    {
+        InputMap.Enable();
+    }
+
+    private void OnDisable()
+    {
+        InputMap.Disable();
+    }
+
+    public static bool IsMoving (out Vector2 direction)
+    {
+        
+        direction = MovementInput;
+        return direction != Vector2.zero;
+    }
 
 }
---
+++ Assets/Code/Scripts/Gameplay/Input/PlayerController.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    //public InputManager inputManager;
+
+    public float mouseSensitivity = 25f;
+    public Transform mesh;
+    public float XSpeed;
+    public float YSpeed;
+    public float CamSpeed;
+    public float PlayerSpeed;
+    public AnimationCurve DecelerationCurve;
+    public float DecelerationSpeed;
+    Rigidbody Rb;
+    float X = 0; // up and down mov
+    float Y = 0; // left and right mov
+    
+
+    private void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        InputManager.InputMap.Overworld.Movement.canceled += Decelerate;
+        InputManager.InputMap.Overworld.Movement.started += StopSlowDownCycle;
+    }
+
+
+
+
+
+
+
+   
+        
+        
+
+
+
+    private void FixedUpdate()
+    {
+        float mouseX = InputManager.InputMap.Overworld.MouseX.ReadValue<float>() * mouseSensitivity *Time.fixedDeltaTime ;
+        float mouseY = InputManager.InputMap.Overworld.MouseY.ReadValue<float>() * mouseSensitivity * Time.fixedDeltaTime ;
+
+        X = Mathf.SmoothDamp(X, mouseY, ref XSpeed, CamSpeed); 
+        Y = Mathf.SmoothDamp(Y, mouseX, ref YSpeed, CamSpeed);
+
+
+        Quaternion rotation = Quaternion.Euler(-X * Time.fixedDeltaTime, Y * Time.fixedDeltaTime, 0); // asse y invertito con (-x)
+        
+        //rb.MoveRotation(Quaternion.Euler (Vector3.SmoothDamp(rb.rotation.eulerAngles, rb.rotation.eulerAngles, ref camSpeed, 1f))) ;
+        Rb.MoveRotation(Rb.rotation * rotation);
+
+        if (InputManager.IsMoving(out Vector2 direction)) 
+        {
+             StopCoroutine(SlowDown());
+            
+            Rb.AddForce(direction.y * Rb.transform.forward* PlayerSpeed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+        
+    }
+
+    void Decelerate(InputAction.CallbackContext obj )
+    {
+        StartCoroutine(SlowDown());
+    }
+
+    IEnumerator SlowDown()
+    {
+
+        Vector3 initialVelocity = Rb.velocity;
+        float time = 0;
+        
+        
+        while (time < 1) 
+        {
+            Rb.velocity = DecelerationCurve.Evaluate(time) * initialVelocity;
+            time += DecelerationSpeed * Time.fixedDeltaTime ; 
+            yield return null;
+            //Debug.Log(DecelerationCurve.Evaluate(time));
+            Debug.Log(Rb.velocity );
+        }
+
+        Rb.velocity = Vector3.zero;
+    }
+
+    void StopSlowDownCycle(InputAction.CallbackContext obj )
+    {
+        Debug.Log("porcodio" );
+        StopAllCoroutines();
+    }
+            
+
+        
+
+            
+               
+    
+
+
+        
+
+}
+
