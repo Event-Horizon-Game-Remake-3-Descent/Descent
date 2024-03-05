36af4b7b420c9b488a2893e70054f4ab60cdb162
DrNox08 <marcopergamena.01@gmail.com>
2024-03-05 23:33:46 +0100

    Added: some ui elements

--- Assets/Code/Scripts/Gameplay/Input/CameraController.cs
+++ Assets/Code/Scripts/Gameplay/Input/CameraController.cs
@@ -8,13 +8,14 @@ public class CameraController : MonoBehaviour
     private float AutoBankingValue = 0f; 
     private float AutobankingVelocity = 0f;
     public float SmoothTime = 0.3f;
+    public float AutoBankingTreshold = 2f;
 
     void Update()
     {
         float mouseX = InputManager.InputMap.Overworld.MouseX.ReadValue<float>();
 
         
-        if (Mathf.Abs(mouseX) > 0.01f)
+        if (Mathf.Abs(mouseX) > AutoBankingTreshold)
         { 
             AutoBankingValue += mouseX > 0 ? -AutoBankingSpeed * Time.deltaTime : AutoBankingSpeed * Time.deltaTime;
             AutoBankingValue = Mathf.Clamp(AutoBankingValue, -10, 10); 
