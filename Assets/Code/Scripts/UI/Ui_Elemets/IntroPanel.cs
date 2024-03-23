93d64490427e3f721634eaa230c81bceeba5e53b
FlavioNovati <f.novati10@gmail.com>
2024-03-23 19:49:24 +0100

    Added Intro sequence comix

--- Assets/Code/Scripts/UI/Ui_Elemets/IntroPanel.cs
+++ Assets/Code/Scripts/UI/Ui_Elemets/IntroPanel.cs
@@ -1,26 +1,49 @@
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class IntroPanel : MonoBehaviour
 {
-    [SerializeField] private RectTransform PanelRectTransform;
-    [SerializeField] private TMP_Text Text;
+    [SerializeField] private List<TMP_Text> TextList = null;
     [SerializeField] private float TextSpeed = 1f;
+    [SerializeField] private int StartChar = 0;
+    [SerializeField] private int StopChar = -1;
 
     private Coroutine AnimationCoroutine = null;
     private WaitForSeconds WaitTime;
     private int TextIndex = 0;
     private bool AnimationEnded = false;
 
+    private void Awake()
+    {
+        if (TextList.Count > 0)
+        {
+            for (int i = 0; i < TextList.Count; i++)
+                TextList[i].maxVisibleCharacters = 0;
+            if (StopChar < 0)
+                StopChar = TextList[0].text.Length;
+        }
+    }
+
     private IEnumerator AnimateText()
     {
-        AnimationEnded = false;
-        while (TextIndex < Text.text.Length)
+        if (TextList == null)
         {
-            TextIndex++;
-            Text.maxVisibleCharacters = TextIndex;
-            yield return WaitTime;
+            AnimationEnded = true;
+            yield break;
+        }
+
+        AnimationEnded = false;
+        for(int i = 0; i < TextList.Count; i++)
+        {
+            TextIndex = StartChar;
+            while (TextIndex < StopChar)
+            {
+                TextIndex++;
+                TextList[i].maxVisibleCharacters = TextIndex;
+                yield return WaitTime;
+            }
         }
         AnimationEnded = true;
     }
@@ -28,7 +51,11 @@ public class IntroPanel : MonoBehaviour
     public void SkipText()
     {
         StopCoroutine(AnimationCoroutine);
-        Text.maxVisibleCharacters = Text.text.Length;
+
+        if (TextList != null)
+            for (int i = 0; i < TextList.Count; i++)
+                TextList[i].maxVisibleCharacters = TextList[i].text.Length;
+
         AnimationEnded = true;
     }
 
